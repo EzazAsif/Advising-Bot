3a09902a981b4b689e2703931f3e33f102066ea2
EzazAsif <147924951+EzazAsif@users.noreply.github.com>
2024-07-01 02:04:50 +0600

    Update ad.py

--- ad.py
+++ ad.py
@@ -43,7 +43,7 @@ async def check_seats(bot):
         soup = BeautifulSoup(response.content, 'html.parser')
         
         # Find the table row that contains the desired courses and check the seats available
-        courses = ['CSE373', 'CSE273', 'CSE445']
+        courses = ['CSE373', 'CSE273', 'CSE445','EEE141']
         table = soup.find('table', {'id': 'offeredCourseTbl'})
         rows = table.find_all('tr')
         
