d8d95df996fb011f0a61ed02829e256c36eca9cd
EzazAsif <147924951+EzazAsif@users.noreply.github.com>
2024-07-01 15:03:14 +0600

    Update ad.py

--- ad.py
+++ ad.py
@@ -43,7 +43,7 @@ async def check_seats(bot):
         soup = BeautifulSoup(response.content, 'html.parser')
         
         # Find the table row that contains the desired courses and check the seats available
-        courses = ['CSE373', 'CSE273', 'CSE445']
+        courses = ['CSE373', 'CSE273', 'CSE445','CSE231']
         table = soup.find('table', {'id': 'offeredCourseTbl'})
         if not table:
             print("Table 'offeredCourseTbl' not found in HTML")
